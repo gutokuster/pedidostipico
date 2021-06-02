6198d7eec994268bb30c65370c270d0652572e02
gkuster <gutokuster@gmail.com>
2021-06-02 14:23:34 -0300

    Configurado namespace dos apps

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('buffet', views.buffet, name='buffet'),
+    path('buffet/', views.buffet, name='buffet'),
     path('cozinha_quente/', views.cozinha_quente, name='cozinha_quente'),
     path('cozinha_fria/', views.cozinha_fria, name='cozinha_fria'),
     path('login/', views.login, name='login'),
