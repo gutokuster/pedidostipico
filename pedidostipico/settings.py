909e24ff62ceec60d6fc2f2ca08a29d553619c64
gkuster <gutokuster@gmail.com>
2021-11-23 13:39:04 -0300

    Alteração DB

--- pedidostipico/settings.py
+++ pedidostipico/settings.py
@@ -79,14 +79,24 @@ WSGI_APPLICATION = 'pedidostipico.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
